c16daac1b34d5faa516ea5755582ccb020968565
kifuan <kifuan@foxmail.com>
2023-03-05 11:59:15 +0800

    feat: 404

--- app.vue
+++ app.vue
@@ -1,21 +1,3 @@
 <template>
-  <div class="flex flex-col py-12 space-y-6">
-    <img src="/avatar.jpg" alt="Avatar" class="self-center rounded-full w-48">
-    <div class="self-center space-x-2 text-slate-900 font-semibold">
-      <span class="text-3xl">摸鱼</span>
-      <span class="text-2xl">@kifuan</span>
-    </div>
-
-    <p class="self-center text-xl">
-      喜欢做点有趣的事情
-    </p>
-
-    <link-button-group />
-  </div>
+  <nuxt-page />
 </template>
-
-<style scoped>
-img {
-  outline: 400px solid rgb(244 244 245);
-}
-</style>
---
+++ error.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="mt-5 text-center p-10 text-4xl leading-10">
+    我甚至没有做另一个页面的打算，访问这里干什么捏？
+  </p>
+</template>
