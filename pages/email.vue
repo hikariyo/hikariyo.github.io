d7321630e48eaf9b7e859a1ad0f27435b852189f
kifuan <kifuan@foxmail.com>
2023-05-13 23:31:43 +0800

    fix: import katex css

--- pages/email.vue
+++ pages/email.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import katex from 'katex'
-// import 'katex/katex.css'
+import 'katex/dist/katex.min.css'
 
 useHead({
   title: 'Email',
 })
 
-
 const math = ref<HTMLDivElement>()
 
 onMounted(() => {
-  katex.render('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', math.value, {
+  katex.render('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', math.value!, {
     throwOnError: false,
     output: 'mathml',
   })
@@ -22,7 +21,7 @@ onMounted(() => {
     <code>
       # Email<br>
       import base64<br>
-      s = 'a2lmdWFuQGZveG1haWwuY' + str(<span ref="math" class="text-2xl"></span>)<br>
+      s = 'a2lmdWFuQGZveG1haWwuY' + str(<span ref="math" class="text-2xl" />)<br>
       base64.b64decode(s.encode())
     </code>
   </div>
