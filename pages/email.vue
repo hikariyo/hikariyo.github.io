96b3543295b93ac4064333d60a4d8ae0bd7695f3
kifuan <kifuan@foxmail.com>
2023-05-14 16:40:18 +0800

    refactor: click to show the result

--- pages/email.vue
+++ pages/email.vue
@@ -6,23 +6,27 @@ useHead({
   title: 'Email',
 })
 
-const math = ref(katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', {
+const math = katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', {
   throwOnError: false,
   output: 'html',
-}))
+})
 
-function handleClick() {
-  math.value = '\'29t\'  # just for fun :)'
-}
+const showResult = ref(false)
 </script>
 
 <template>
   <div class="py-14 px-4 xl:px-20 text-xl">
     <code>
       # Email<br>
-      # <a href="javascript:void(0)" class="underline decoration-sky-300 hover:decoration-sky-500" @click="handleClick">Click me</a> to show the result <br>
+      # <a href="javascript:void(0)" class="underline decoration-sky-300 hover:decoration-sky-500" @click="showResult = true">Click me</a> to show the result <br>
       import base64<br>
-      s = 'a2lmdWFuQGZveG1haWwuY' + <span class="text-xl" v-html="math" /><br>
+      <template v-if="showResult">
+        s = 'a2lmdWFuQGZveG1haWwuY29t'  # just for fun :)
+      </template>
+      <template v-else>
+        s = 'a2lmdWFuQGZveG1haWwuY' + <span class="text-xl" v-html="math" />
+      </template>
+      <br>
       base64.b64decode(s.encode())
     </code>
   </div>
