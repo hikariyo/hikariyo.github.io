dde7fbce79f683877992c1b05dff21b794d8e87a
kifuan <kifuan@foxmail.com>
2023-05-14 15:41:49 +0800

    feat: use `Noto Sans SC`

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
   <nuxt-page />
 </template>
+
+<style>
+html {
+  font-family: -apple-system, 'Noto Sans SC', BlinkMacSystemFont, 'Segoe UI', sans-serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
   app: {
     head: {
       title: '摸鱼 @kifuan',
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+SC' },
+      ],
     },
   },
   tailwindcss: {
