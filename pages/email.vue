c2cbaeb4a7a9b520573e07aabe1a64955a9c0c86
kifuan <kifuan@foxmail.com>
2023-05-14 16:21:32 +0800

    feat: put the result in comment

--- pages/email.vue
+++ pages/email.vue
@@ -6,7 +6,6 @@ useHead({
   title: 'Email',
 })
 
-// 29t, the result of left part is 0.
 const math = katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', {
   throwOnError: false,
   output: 'html',
@@ -15,8 +14,10 @@ const math = katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x
 
 <template>
   <div class="py-14 px-4 xl:px-20 text-xl">
+    <!-- 29t, the result of left part is 0. -->
     <code>
       # Email<br>
+      # P.S. Inspect Element <br>
       import base64<br>
       s = 'a2lmdWFuQGZveG1haWwuY' + str(<span class="text-xl" v-html="math" />)<br>
       base64.b64decode(s.encode())
