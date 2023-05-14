04a001fffa875642cf9a52a7530b7e11eb6447fc
kifuan <kifuan@foxmail.com>
2023-05-14 16:37:22 +0800

    feat: click to show the result

--- pages/email.vue
+++ pages/email.vue
@@ -6,20 +6,23 @@ useHead({
   title: 'Email',
 })
 
-const math = katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', {
+const math = ref(katex.renderToString('\\int_{-t}^t(xe^{3x^2}\\cos\\frac{3}{7}\\pi x+\\frac{29}{2})\\mathrm{d}x', {
   throwOnError: false,
   output: 'html',
-})
+}))
+
+function handleClick() {
+  math.value = '\'29t\'  # just for fun :)'
+}
 </script>
 
 <template>
   <div class="py-14 px-4 xl:px-20 text-xl">
-    <!-- 29t, the result of left part is 0. -->
     <code>
       # Email<br>
-      # P.S. Inspect Element <br>
+      # <a href="javascript:void(0)" class="underline decoration-sky-300 hover:decoration-sky-500" @click="handleClick">Click me</a> to show the result <br>
       import base64<br>
-      s = 'a2lmdWFuQGZveG1haWwuY' + str(<span class="text-xl" v-html="math" />)<br>
+      s = 'a2lmdWFuQGZveG1haWwuY' + <span class="text-xl" v-html="math" /><br>
       base64.b64decode(s.encode())
     </code>
   </div>
